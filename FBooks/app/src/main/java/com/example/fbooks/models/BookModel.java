ac05aecbfc09aed9a533cdfb61b59c35a87440fa
tamayoram <61298214+tamayoram@users.noreply.github.com>
2020-11-01 21:16:08 -0500

    Proyecto Firebase

---
+++ FBooks/app/src/main/java/com/example/fbooks/models/BookModel.java
@@ -0,0 +1,61 @@
+package com.example.fbooks.models;
+
+import java.io.Serializable;
+
+public class BookModel implements Serializable {
+
+    private String id;
+    private String code;
+    private String description;
+    private String title;
+    private String imageUrl;
+    private boolean available;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
