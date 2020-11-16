5c0467100c7c22f75db9a949d4fcf74221d376bf
tamayoram <61298214+tamayoram@users.noreply.github.com>
2020-11-16 07:19:55 -0500

    Versión para momento II conexión Firebase y lista de colores random

--- FBooks/app/src/main/java/com/example/fbooks/adapters/BookAdapter.java
+++ FBooks/app/src/main/java/com/example/fbooks/adapters/BookAdapter.java
@@ -1,17 +1,20 @@
 package com.example.fbooks.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.fbooks.R;
 import com.example.fbooks.models.BookModel;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class BookAdapter extends BaseAdapter {
 
@@ -48,10 +51,21 @@ public class BookAdapter extends BaseAdapter {
         TextView tv_book_list_item_code=view.findViewById(R.id.tv_book_list_item_code);
         TextView tv_book_list_item_title=view.findViewById(R.id.tv_book_list_item_title);
         EditText et_book_list_item_description= view.findViewById(R.id.et_book_list_item_description);
+        ImageView iv_book_list_item_image=view.findViewById(R.id.iv_book_list_item_image);
 
         tv_book_list_item_code.setText(getItem(i).getCode());
         tv_book_list_item_title.setText(getItem(i).getTitle());
         et_book_list_item_description.setText(getItem(i).getDescription());
+
+        String[] colores = {"#ff33b5e5", "#ffff8800", "#ff99cc00", "#ffff4444", "#ff0099cc", "#ff669900", "#ffaa66cc", "#ffffbb33"};
+        Random r =new Random();
+        int randomNumber = r.nextInt(colores.length);
+        view.setBackgroundColor(Color.parseColor(colores[randomNumber]));
+
+        String[] iconos = {"ic_library_books_black_18dp", "ic_book_online_black_18dp","ic_import_contacts_black_18dp","ic_bookmark_border_black_18dp","ic_menu_book_black_18dp"};
+        randomNumber = r.nextInt(iconos.length);
+        int icono = view.getResources().getIdentifier("com.example.fbooks:drawable/" + iconos[randomNumber], null, null);
+        iv_book_list_item_image.setImageResource(icono);
 
         return view;
     }
